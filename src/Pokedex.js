c67d1a05418132d6f2cb9b7feb259131cc41d7c5
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2021-09-09 09:08:54 -0700

    react pokedex exercise

---
+++ src/Pokedex.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Pokecard from "./Pokecard";
+import './Pokedex.css'
+
+function Pokedex(props) {
+    let winMessage = null;
+    if(props.isWinner) {
+        winMessage = <p className="Pokedex-winner">THIS HAND WINS!</p>;
+    }
+
+    return (
+        <div className="Pokedex">
+            <h2 className="Pokedex-title">Pokedex</h2>
+            <div className="Pokedex-cards">
+                {props.pokemon.map(poke => (
+                    <Pokecard
+                    id={poke.id}
+                    name={poke.name}
+                    type={poke.type}
+                    exp={poke.base_experience}
+                    />
+                ))}
+            </div>
+            <h4>Total experience: {props.exp}</h4>
+            {winMessage}
+        </div>
+    );
+}
+
+export default Pokedex;
